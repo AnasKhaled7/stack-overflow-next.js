e50c8e2b2a3c6f442b710178d8521b8a8b445240
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-01-15 22:40:01 +0200

    Add Home page UI

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,6 +5,46 @@ import LocalSearchbox from "@/components/shared/search/LocalSearchbox";
 import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to use React Query?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "React Query" },
+    ],
+    author: {
+      _id: "123",
+      name: "John Doe",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 10,
+    views: 1110100,
+    answers: [],
+    createdAt: new Date("2023-10-10T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "Next.js" },
+    ],
+    author: {
+      _id: "456",
+      name: "Jane Doe",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 15,
+    views: 200,
+    answers: [],
+    createdAt: new Date("2021-10-10T12:00:00.000Z"),
+  },
+];
 
 export default function Home() {
   return (
@@ -36,6 +76,31 @@ export default function Home() {
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
