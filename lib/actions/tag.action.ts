ace232bd764d011569260af0f276949bfe2677ca
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-02-04 18:21:24 +0200

    Add getAllTags function to retrieve all tags

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -3,6 +3,7 @@
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
 import { GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -21,6 +22,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "1", name: "tag" },
       { _id: "2", name: "tag2" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags() {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find();
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
