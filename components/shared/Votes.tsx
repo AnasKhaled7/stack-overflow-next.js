a931bede612a06a1ed15620ed070a71114445074
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-02-11 02:51:39 +0200

    Implement Save Question Action

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -5,6 +5,7 @@ import {
   upvoteQuestion,
   downvoteQuestion,
 } from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -32,7 +33,13 @@ const Votes = ({
 }: Props) => {
   const pathname = usePathname();
 
-  const handleSave = () => {};
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      questionId: JSON.parse(itemId),
+      userId: JSON.parse(userId),
+      path: pathname,
+    });
+  };
 
   const handleVote = async (action: string) => {
     if (!userId) return;
