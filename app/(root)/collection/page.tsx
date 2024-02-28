a6431853e3329af21380fda59cb7b0fff036ded9
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-02-28 02:29:52 +0200

    Add search functionality in Collections page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -14,6 +14,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
 
   const result = await getSavedQuestions({
     clerkId: userId,
+    searchQuery: searchParams.q,
   });
 
   return (
@@ -22,7 +23,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
-          route="/"
+          route="/collection"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search for questions"
