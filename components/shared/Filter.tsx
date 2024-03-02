e562cbd20627ab977b4dc5a8e2379b98da183388
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-03-02 17:10:17 +0200

    Add filter functionality to community page

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -8,8 +8,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
-interface props {
+interface Props {
   filters: {
     name: string;
     value: string;
@@ -18,10 +20,28 @@ interface props {
   containerClasses?: string;
 }
 
-const Filter = ({ filters, otherClasses, containerClasses }: props) => {
+const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const paramFilter = searchParams.get("filter");
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
     <div className={`relative ${containerClasses}`}>
-      <Select>
+      <Select
+        onValueChange={handleUpdateParams}
+        defaultValue={paramFilter || undefined}
+      >
         <SelectTrigger
           className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
@@ -29,11 +49,15 @@ const Filter = ({ filters, otherClasses, containerClasses }: props) => {
             <SelectValue placeholder="Select a Filter" />
           </div>
         </SelectTrigger>
-        <SelectContent>
+        <SelectContent className="text-dark500_light700 small-regular border-none bg-light-900 dark:bg-dark-300">
           <SelectGroup>
-            {filters.map((filter) => (
-              <SelectItem key={filter.value} value={filter.value}>
-                {filter.name}
+            {filters.map((item) => (
+              <SelectItem
+                key={item.value}
+                value={item.value}
+                className="cursor-pointer focus:bg-light-800 dark:focus:bg-dark-400"
+              >
+                {item.name}
               </SelectItem>
             ))}
           </SelectGroup>
